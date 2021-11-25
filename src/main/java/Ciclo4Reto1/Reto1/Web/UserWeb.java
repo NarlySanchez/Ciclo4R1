4598634ceef2116d4894379fd64f998d67193a57
Narly Beatriz Sánchez Caviedes <narly.sanchez.mt@correo.usa.edu.co>
2021-11-25 10:52:58 -0500

    Reto 1 Ciclo 4

---
+++ src/main/java/Ciclo4Reto1/Reto1/Web/UserWeb.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ciclo4Reto1.Reto1.Web;
+
+import Ciclo4Reto1.Reto1.Model.User;
+import Ciclo4Reto1.Reto1.Service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author nbsc1
+ * Reto 1 Ciclo 4
+ */
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+
+public class UserWeb {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+    
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registrar(@RequestBody User user) {
+        return userService.registrar(user);
+    }
+    
+    @GetMapping("/{email}/{password}")
+    public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
+        return userService.autenticarUsuario(email, password);
+    }
+    
+    @GetMapping("/{email}")
+    public boolean existeEmail(@PathVariable("email") String email) {
+        return userService.existeEmail(email);
+    }
+    
+}
+
